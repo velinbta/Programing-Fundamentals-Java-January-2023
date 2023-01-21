ee01622e505e84205fdc79fc2b1073ab82c8349c
velinbta <122949696+velinbta@users.noreply.github.com>
2023-01-22 01:48:35 +0200

    Added solution

---
+++ Arrays/Train-01/Train_01.java
@@ -0,0 +1,30 @@
+package ArraysExercises;
+
+import java.util.Scanner;
+
+public class Train_01_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine()); // Брой на вагоните
+
+        int[] passengersArr = new int[n];
+
+        for (int i = 0; i < passengersArr.length; i++) {
+            // Добавям ги към масива
+            passengersArr[i] = Integer.parseInt(scanner.nextLine());
+        }
+
+        int totalPassengers = 0;
+        for (int i = 0; i < passengersArr.length; i++) {
+            totalPassengers += passengersArr[i]; // Пресмятам общо, колко са
+            // Принтирам всеки по отделно
+            System.out.print(passengersArr[i] + " ");
+
+        }
+        // Свалям на нов ред и принтирам общо колко са
+        System.out.println();
+        System.out.println(totalPassengers);
+
+    }
+}
