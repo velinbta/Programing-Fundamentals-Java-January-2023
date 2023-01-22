6388de520b53b47b51367e48d71bcd8644fb4372
velinbta <122949696+velinbta@users.noreply.github.com>
2023-01-23 00:53:24 +0200

    Solution added

---
+++ Methods/MiddleCharacters-06/MiddleCharacters_06_1.java
@@ -0,0 +1,35 @@
+package MethodsExercises;
+
+import java.util.Scanner;
+
+public class MiddleCharacters_06 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+
+        // Методът намира “средната или средните" букви
+        printMiddleCharacters(input);
+
+    }
+
+    public static void printMiddleCharacters (String input) {
+        // От тук ще взема позицията на символа
+        // или символите в зависимост дали са четни или не
+        int middle = input.length() / 2;
+
+        // Проверявам дали е четен брой
+        if (input.length() % 2 == 0) {
+
+            System.out.print (input.charAt(middle - 1) + "" + input.charAt(middle));
+
+        } else {
+
+            System.out.print (input.charAt(middle));
+
+        }
+
+    }
+
+
+}
---
+++ Methods/MiddleCharacters-06/MiddleCharacters_06_2.java
@@ -0,0 +1,35 @@
+package MethodsExercises;
+
+import java.util.Scanner;
+
+public class MiddleCharacters_06_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Протичам текста, като масив от символи
+        char[] chArr = scanner.nextLine().toCharArray();
+
+        // Методът намира “средната или средните" букви
+        printMiddleChars(chArr);
+
+    }
+
+    private static void printMiddleChars(char[] findMiddleChars) {
+        // От тук ще взема позицията на символа
+        // или символите в зависимост дали са четни или не
+        int middle = findMiddleChars.length / 2;
+
+        // Проверявам дали е четен брой
+        if (findMiddleChars.length % 2 == 0) {
+            // Ако е четен взимам символа преди средния и средния
+            System.out.println(findMiddleChars[middle - 1] + "" + findMiddleChars[middle]);
+
+        } else {
+            // Ако е нечетен - взимам средния
+            System.out.println(findMiddleChars[middle]);
+
+        }
+
+    }
+
+}
