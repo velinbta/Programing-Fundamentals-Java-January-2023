56eeb74b31a0bafc29a6488b1e3b5bfe647ffd49
velinbta <122949696+velinbta@users.noreply.github.com>
2023-01-22 02:09:31 +0200

    Added solution

---
+++ Arrays/CommonElements-02/CommonElements_02_2.java
@@ -0,0 +1,29 @@
+package ArraysExercises;
+
+import java.util.Scanner;
+
+public class CommonElements_02_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Прочитам два масива от конзолата
+        String[] firstArr = scanner.nextLine().split("\\s+");
+        String[] secondArr = scanner.nextLine().split("\\s+");
+        // Понеже по условие, трябва да се проверят общите от втория с първия,
+        // започвам от втория
+        for (String sec : secondArr) {
+
+            for (String fir : firstArr) {
+
+                if (sec.equals(fir)) {
+                    // Принтирам само ако са еднакви
+                    System.out.printf("%s ", sec);
+
+                }
+
+            }
+
+        }
+
+
+    }
+}
---
+++ Arrays/ZigZagArrays-03/ZigZagArrays_03.java
@@ -0,0 +1,54 @@
+package ArraysExercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ZigZagArrays_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int lines = Integer.parseInt(scanner.nextLine()); // Брой редове
+        // Съзвадам два масива
+        int[] firstArr = new int[lines];
+        int[] secondArr = new int[lines];
+
+        for (int i = 0; i < lines; i++) {
+
+            String numbers = scanner.nextLine();
+            // Това, което прочитам го парсвам към int, като го отделям по whitespace+ "\\s+"
+            int firstNumber = Integer.parseInt(numbers.split("\\s+")[0]);
+            int secondNumber = Integer.parseInt(numbers.split("\\s+")[1]);
+
+            // На четна итерация, добавям към първия масив - първото число, към втория - второто
+            // на нечетна - обратното
+            if (i % 2 == 0) {
+                firstArr[i] = firstNumber;
+                secondArr[i] = secondNumber;
+
+            } else {
+
+                firstArr[i] = secondNumber;
+                secondArr[i] = firstNumber;
+
+            }
+
+        }
+
+        for (int j : firstArr) {
+
+            System.out.print(j + " ");
+        }
+
+        System.out.println();
+
+        for (int j : secondArr) {
+
+            System.out.print(j + " ");
+        }
+        // Алтернативен начин на принтиране:
+//        System.out.println(Arrays.toString(Arrays.stream(firstArr).toArray()).replaceAll("[\\[\\],]", ""));
+//        System.out.println(Arrays.toString(Arrays.stream(secondArr).toArray()).replaceAll("[\\[\\],]", ""));
+
+
+    }
+}
