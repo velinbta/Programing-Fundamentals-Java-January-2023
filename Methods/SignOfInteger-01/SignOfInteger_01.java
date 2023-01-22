1c5c80e4cfb718918acdbb75e1a9b16e2351e2f7
velinbta <122949696+velinbta@users.noreply.github.com>
2023-01-22 05:58:19 +0200

    Added solution

---
+++ Methods/SignOfInteger-01/SignOfInteger_01.java
@@ -0,0 +1,31 @@
+package MethodsLab;
+
+import java.util.Scanner;
+
+public class SignOfInteger_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number = Integer.parseInt(scanner.nextLine());
+        printNumSign(number); // Метод, който ми притира знака на числото
+
+    }
+
+    public static void printNumSign(int num) {
+
+        if (num > 0) {
+
+            System.out.printf("The number %d is positive.", num);
+
+        } else if (num < 0) {
+
+            System.out.printf("The number %d is negative.", num);
+
+        } else {
+
+            System.out.println("The number 0 is zero.");
+
+        }
+
+    }
+}
