0310702059bc2d835cace6c3185a7510e67624b3
velinbta <122949696+velinbta@users.noreply.github.com>
2023-01-22 07:06:08 +0200

    Solution added

---
+++ Methods/CalculateRectangleArea-06/CalculateRectangleArea_06.java
@@ -0,0 +1,24 @@
+package MethodsLab;
+
+import java.util.Scanner;
+
+public class CalculateRectangleArea_06_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Прочитам ширина и дължина
+        double width = Double.parseDouble(scanner.nextLine());
+        double length = Double.parseDouble(scanner.nextLine());
+
+        // Методът ми връща ширината и дължината умножени
+        double area = getRectangleArea(width, length);
+
+        System.out.printf("%.0f", area);
+
+    }
+
+    private static double getRectangleArea(double width, double length) {
+
+        return width * length;
+        
+    }
+}
