ec6d835b01b1b6b9e045eec5037c29953f57d554
velinbta <122949696+velinbta@users.noreply.github.com>
2023-01-31 12:18:09 +0200

    Solution added

---
+++ BasicSyntax/TriangleOfNumbers-08/README.txt
@@ -0,0 +1,36 @@
+8. Triangle of Numbers
+Write a program that receives a number - n and prints a triangle from 1 to n as in the examples.
+
+Constraints
+n will be in the interval [1...20].
+
+Examples:
+
+Input 1:
+3
+
+Output 1:
+1
+2 2 
+3 3 3
+
+Input 2:
+5
+
+Output 2:
+1
+2 2 
+3 3 3
+4 4 4 4
+5 5 5 5 5
+
+Input 3:
+6
+
+Output 3:
+1
+2 2 
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
---
+++ BasicSyntax/TriangleOfNumbers-08/TriangleOfNumbers_08.java
@@ -0,0 +1,25 @@
+package BasicSyntax;
+
+import java.util.Scanner;
+
+public class TriangleOfNumbers_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            // Вложеният цикъл ще принтира всяко нарастващо i - колкото е стойността му
+            for (int j = 1; j <= i; j++) {
+
+                System.out.print(i + " ");
+
+            }
+
+            System.out.println(); // <- Свалям на нов ред
+
+        }
+
+    }
+
+}
