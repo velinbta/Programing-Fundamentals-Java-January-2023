bc71b3881d0630510e846bbaed389933be8e25f8
velinbta <122949696+velinbta@users.noreply.github.com>
2023-02-04 04:45:11 +0200

    Solution added

---
+++ ObjectsClasses/BigFactorial-03/BigFactorial_03.java
@@ -0,0 +1,24 @@
+package ObjectsClasses;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigFactorial_03 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        BigInteger factorialOfN = BigInteger.valueOf(1);
+
+        for (int i = 1; i <= n; i++) { // <- От 1 до n
+
+            factorialOfN = factorialOfN.multiply(BigInteger.valueOf(i)); // <- factorialOfN * i
+
+        }
+
+        System.out.println(factorialOfN);
+
+    }
+
+}
---
+++ ObjectsClasses/BigFactorial-03/README.txt
@@ -0,0 +1,16 @@
+3. Big Factorial
+You will receive N - the number in the range [0 - 1000]. Calculate the Factorial of N and print the result.
+
+Examples:
+
+Input 1:
+5
+
+Output 1:
+120
+
+Input 2:
+50
+
+Output 2:
+30414093201713378043612608166064768844377641568960512000000000000
