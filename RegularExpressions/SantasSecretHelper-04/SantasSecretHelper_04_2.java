412094bb4012f2a76d602f47e448bfb45aee397c
velinbta <122949696+velinbta@users.noreply.github.com>
2023-03-28 23:26:12 +0300

    Solution added

--- RegularExpressions/SantasSecretHelper-04/SantasSecretHelper_04_2.java
+++ RegularExpressions/SantasSecretHelper-04/SantasSecretHelper_04_2.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SantasSecretHelper_04 {
+public class SantasSecretHelper_04_2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -18,8 +18,13 @@ public class SantasSecretHelper_04 {
         List<String> childrenList = new ArrayList<>(); // <- Списък с послушните имена на деца
         while (!input.equals("end")) {
 
-            // Дешифрирам съобщението
-            String decryptedMessage = getDecryptedMessageBySubtractingKey(input, decryptionKey);
+            // Създавам масив от чарове и издваждам от всеки чар "ключа - decryptionKey"
+            Character[] decryptedCharactersArr = input.chars().mapToObj(e -> (char) e)
+                    .map(e -> (char) (e - decryptionKey)).toArray(Character[]::new);
+
+            String decryptedMessage = Arrays.toString(decryptedCharactersArr)
+                    .replaceAll("[\\[\\]]", "")
+                    .replaceAll(", ", ""); // <- Запазвам, ако има запетая в масива
 
             String regex = "@(?<name>[A-Za-z]+)[^@\\-!:>]*(!G!)"; // <- Име и !G! - добро поведение
             Pattern pattern = Pattern.compile(regex);
@@ -35,23 +40,6 @@ public class SantasSecretHelper_04 {
 
         childrenList.forEach(child -> System.out.println(child));
 
-    }
-
-    private static String getDecryptedMessageBySubtractingKey(String decrypted, int key) {
-        // Изважда определения брой "ключове“, от всеки символ и връща дешифрирания String
-        char[] decryptedArr = decrypted.toCharArray();
-
-        for (int i = 0; i < decryptedArr.length; i++) {
-
-            char currentChar = decrypted.charAt(i);
-            char decryptedChar = (char) (currentChar - key);
-
-            decryptedArr[i] = decryptedChar;
-
-        }
-
-        return Arrays.toString(decryptedArr).replaceAll("[\\[\\]]", "")
-                .replaceAll("(, )", ""); // <- Запазвам запетаята, ако е char!
     }
 
 }
